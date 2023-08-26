f2e9177617d6c8889fb27278eba4a7ffa8feba26
Bhavishjb <bhavishkarkera22@gmail.com>
2023-08-26 11:47:20 +0530

    Week-4 Creating API

--- Week-3/mysite/polls/urls.py
+++ Week-3/mysite/polls/urls.py
@@ -10,23 +10,11 @@ app_name ="polls"
 # router.register('',QuestionViewSet, basename="questions")
 # print("router urls", router.urls)
 
-# urlpatterns = router.urls
-
-
-    # path("", views.index, name = "index"),
-    # path("specifics/<int:question_id>/", views.detail, name="detail"),
-    # path("<int:question_id>/", views.detail, name="detail"),
-    # path("<int:question_id>/results/", views.results, name="results"),
-    # path("<int:question_id>/vote/", views.vote, name="vote")
-    
-
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/",views.ResultsView.as_view(), name="results"),
+    path("", views.index, name = "index"),
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
-    path("get_or_post_polls/", views.get_or_post_polls,name="get_or_post_polls"),
-    path("get_or_put_inc/<int:id>/", views.get_or_put_inc, name="get_or_put_inc"),
-    path("get_polls_by_tags/",views.get_polls_by_tags,name="get_polls_by_tags"),
-    path("tags/", views.get_tags, name="get_tags"),
 ]
+    
